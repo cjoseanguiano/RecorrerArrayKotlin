52adb6b1929af5f7a7ad7437c62aa43ef85c82d8
Carlos Anguiano <carlos.anguiano@gmail.com>
2017-05-23 21:30:34 -0500

    Recorrer Array Kotlin

---
+++ src/array/Main.kt
@@ -0,0 +1,17 @@
+package array
+
+/**
+ * Created by carlosjoseanguiano on 23/05/17.
+ */
+
+fun main(args: Array<String>) {
+
+    val list = ArrayList<String>()
+    list.add("Hola A")
+    list.add("Hola B")
+
+    for (continuar in list) {
+         println(continuar)
+    }
+    println("Hola Mundo")
+}
